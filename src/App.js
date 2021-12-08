5f31d5bf999180991185fe342b87456804562af6
kareem nour <kimzcodes@gmail.com>
2021-12-09 01:13:35 +0200

    change provider place

--- src/App.js
+++ src/App.js
@@ -1,71 +1,17 @@
-import { useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import store from './store/index';
+
+import { Provider } from 'react-redux';
+import Counter from './components/Counter';
+
 import './App.css';
-
 function App() {
-  const dispatch = useDispatch();
-  const globalState = useSelector((state) => state);
-
-  const counterOperation = useCallback(
-    (type, payload) => {
-      dispatch({ type, payload });
-    },
-    [dispatch]
-  );
-
-  //react init
-  //useEffect run -> action -> 10
-  //re render
-  //re revalute
-  //useEffect run -> action -> 10
-  //re render
-  //re revalute
-
-  useEffect(() => counterOperation('increase', 10), [counterOperation]);
-
-  const handlerCounterValue = (value) => {
-    if (value < 1) {
-      return 'no number';
-    }
-    return value;
-  };
-
-  const toggleCounter = () => {
-    console.log('sad');
-    dispatch({ type: 'toggleCounter' });
-  };
-
   return (
-    <div className='App'>
-      <h1>Hello Redux Basic</h1>
-      {globalState.showCounter && (
-        <>
-          <div className='counter'>
-            Counter: {handlerCounterValue(globalState.value)}
-          </div>
-          <div>
-            <button
-              className='btn'
-              onClick={() => counterOperation('increase', 5)}
-            >
-              increase +
-            </button>
-            <button
-              className='btn'
-              onClick={() => counterOperation('decrease', 2)}
-            >
-              decrease -
-            </button>
-          </div>
-        </>
-      )}
-
-      <div>
-        <button className='btn' onClick={toggleCounter}>
-          Hide/Show Counter Number
-        </button>
-      </div>
-    </div>
+    <>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+      {/* another components */}
+    </>
   );
 }
 
---
+++ src/components/Counter.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const globalState = useSelector((state) => state);
+
+  const counterOperation = useCallback(
+    (type, payload) => {
+      dispatch({ type, payload });
+    },
+    [dispatch]
+  );
+
+  //react init
+  //useEffect run -> action -> 10
+  //re render
+  //re revalute
+  //useEffect run -> action -> 10
+  //re render
+  //re revalute
+
+  useEffect(() => counterOperation('increase', 10), [counterOperation]);
+
+  const handlerCounterValue = (value) => {
+    if (value < 1) {
+      return 'no number';
+    }
+    return value;
+  };
+
+  const toggleCounter = () => {
+    console.log('sad');
+    dispatch({ type: 'toggleCounter' });
+  };
+
+  return (
+    <div className='App'>
+      <h1>Hello Redux Basic</h1>
+      {globalState.showCounter && (
+        <>
+          <div className='counter'>
+            Counter: {handlerCounterValue(globalState.value)}
+          </div>
+          <div>
+            <button
+              className='btn'
+              onClick={() => counterOperation('increase', 5)}
+            >
+              increase +
+            </button>
+            <button
+              className='btn'
+              onClick={() => counterOperation('decrease', 2)}
+            >
+              decrease -
+            </button>
+          </div>
+        </>
+      )}
+
+      <div>
+        <button className='btn' onClick={toggleCounter}>
+          Hide/Show Counter Number
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
