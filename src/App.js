f329d25616c843d7e0386ecbeac94d95df0b59f1
kareem nour <kimzcodes@gmail.com>
2021-12-08 23:25:19 +0200

    get state from store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const state = useSelector((state) => {
+    if (state.value < 1) {
+      return 'no number';
+    }
+    return state.value;
+  });
+
+  const increase = () => {
+    const action = { type: 'increase', payload: 4 };
+    dispatch(action);
+  };
+
+  const decrease = () => {
+    const action = { type: 'decrease', payload: 2 };
+    dispatch(action);
+  };
+
   return (
     <div className='App'>
       <h1>Hello Redux Basic</h1>
-      <div className='counter'>Counter: 0</div>
+      <div className='counter'>Counter: {state}</div>
       <div>
-        <button className='btn'>increase +</button>
-        <button className='btn'>decrease -</button>
+        <button className='btn' onClick={increase}>
+          increase +
+        </button>
+        <button className='btn' onClick={decrease}>
+          decrease -
+        </button>
       </div>
+      )}
       <div>
         <button className='btn'>Hide/Show Counter Number</button>
       </div>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'redux';
 
 const initState = { value: 0 };
 const counterReducer = (state = initState, action) => {
+  if (action.type === 'increase') {
+    return { ...state, value: state.value + action.payload };
+  }
+
+  if (action.type === 'decrease') {
+    return { ...state, value: state.value - action.payload };
+  }
+
   return state;
 };
 
 //app init -> run store -> action(non)-> counter reducer -> 0
+//user click increase -> action(increase) -> counter reducer -> 1
 
 const store = createStore(counterReducer);
 
