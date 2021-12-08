40af9bdb76c882581981697eb93e1f5f6d9970a8
kareem nour <kimzcodes@gmail.com>
2021-12-08 21:38:26 +0200

    init the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello Redux Basic</h1>
+      <div className='counter'>Counter: 0</div>;
+      <div>
+        <button className='btn'>increase +</button>
+        <button className='btn'>decrease -</button>
+      </div>
+      <div>
+        <button className='btn'>Hide/Show Counter Number</button>
+      </div>
     </div>
   );
 }
