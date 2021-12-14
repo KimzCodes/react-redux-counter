e925ad53ff27263b7c01882de3326c64fe46b760
kareem nour <kimzcodes@gmail.com>
2021-12-14 21:07:23 +0200

    finish

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 //actions
 import { increase, decrease } from '../store/counterSlice';
 import { logIn, logOut } from '../store/authSlice';
 
+//useEffect -> dispatch
+//update state
+//re render
+
+//useEffect ->2
+//-> dispatch
+//re render
+
+//useEffect -> 3
+//-> dispatch
+///re evalute -> 4
+//re render
+
+//useEffect -> 4
+
+///re evalute -> ref -> 1  /ref -> 2
+//re evalute -> counterHandler -> 3
+
 const Counter = () => {
   const globalState = useSelector((state) => state);
   const dispatch = useDispatch();
+
+  const counterHandler = useCallback(
+    (type, value) => {
+      if (type === 'increase') {
+        dispatch(increase(value));
+      } else {
+        dispatch(decrease(value));
+      }
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    counterHandler('increase', 15);
+  }, [counterHandler]);
 
   const isLoggedIn = () => {
     return globalState.auth.isLoggedIn;
@@ -28,10 +61,16 @@ const Counter = () => {
         <>
           <div className='counter'>Counter: {globalState.counter.value}</div>
           <div>
-            <button className='btn' onClick={() => dispatch(increase(5))}>
+            <button
+              className='btn'
+              onClick={() => counterHandler('increase', 5)}
+            >
               increase +
             </button>
-            <button className='btn' onClick={() => dispatch(decrease(2))}>
+            <button
+              className='btn'
+              onClick={() => counterHandler('decrease', 2)}
+            >
               decrease -
             </button>
           </div>
